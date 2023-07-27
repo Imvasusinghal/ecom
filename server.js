ca270445f4a2ebfb855f9c067e462efe1a7c4eb6
Imvasusinghal <80517041+Imvasusinghal@users.noreply.github.com>
2023-07-27 19:13:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 //CONFIGURE env
 dotenv.config();
 
 //DATABASE CONFIG
 connectDB();
+
+//ES MODULE FIX
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 
 //REST OBJECT
 const app = express();
