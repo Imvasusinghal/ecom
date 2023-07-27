7799c85191d0dbd6771504a5da18135996dade9c
Imvasusinghal <80517041+Imvasusinghal@users.noreply.github.com>
2023-07-27 18:46:21 +0530

    final code
    ecommerce web deployment

---
+++ routes/authRoute.js
@@ -0,0 +1,52 @@
+import express from "express";
+import {
+    registerController,
+    loginController,
+    testController,
+    forgotPasswordController,
+    getOrdersController,
+    getAllOrdersController,
+    orderStatusController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import { updateProfileController } from './../controllers/authController.js';
+
+//CREATING ROUTER OBJECT
+const router = express.Router();
+
+//ROUTING
+//REGIDTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post('/login', loginController);
+
+//FORGOT PASSWORD || POST
+router.post('/forgot-password', forgotPasswordController)
+
+//TEST ROUTES
+router.get("/test", requireSignIn, isAdmin, testController);      //CAN CREATE ANY NUMBER OF MIDDLE WARE BETWEEN TEST AND CONTROLLER
+
+//PROTECTED USER ROUTE AUTH
+router.get('/user-auth', requireSignIn, (req,res) => {
+    res.status(200).send({ ok: true});
+});
+
+//PROTECTED ADMIN ROUTE AUTH
+router.get('/admin-auth', requireSignIn, isAdmin, (req,res) => {
+    res.status(200).send({ ok: true});
+});
+
+//UPDATE PROFILE
+router.put("/profile", requireSignIn, updateProfileController);
+
+//ORDERS
+router.get('/orders', requireSignIn, getOrdersController);
+
+//ALL ORDERS
+router.get('/all-orders', requireSignIn,isAdmin, getAllOrdersController);
+
+//ORDER STATUS UPDATE
+router.put('/order-status/:orderId', requireSignIn, isAdmin, orderStatusController)
+
+export default router;
